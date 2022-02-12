bed4606d851fe3bfe9349bc5a6dea5c682960b9a
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-02-12 12:32:31 -0300

    Adicionando Exceptions

--- TesteBackendEnContact/Core/Domain/ContactBook/Company.cs
+++ TesteBackendEnContact/Core/Domain/ContactBook/Company.cs
@@ -1,4 +1,9 @@
-﻿using TesteBackendEnContact.Core.Interface.ContactBook.Company;
+﻿
+using System.Collections.Generic;
+using TesteBackendEnContact.Core.Interface.ContactBook.Company;
+using TesteBackendEnContact.Core.Interface.ContactBook;
+using TesteBackendEnContact.Core.Exceptions;
+using TesteBackendEnContact.Core.Validators;
 
 namespace TesteBackendEnContact.Core.Domain.ContactBook.Company
 {
@@ -7,12 +12,39 @@ namespace TesteBackendEnContact.Core.Domain.ContactBook.Company
         public int Id { get; private set; }
         public int ContactBookId { get; private set; }
         public string Name { get; private set; }
+        private List<string> _errors;
 
+        protected Company()
+        {
+        }
         public Company(int id, int contactBookId, string name)
         {
             Id = id;
             ContactBookId = contactBookId;
             Name = name;
+            _errors = new List<string>();
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public bool Validate()
+        {
+            var validator = new CompanyValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+                
+                throw new DomainException("Alguns campos estão invalidos, por favor corrija-os!", _errors);
+            }
+
+            return true;
         }
     }
 }
--- TesteBackendEnContact/Core/Domain/ContactBook/ContactBook.cs
+++ TesteBackendEnContact/Core/Domain/ContactBook/ContactBook.cs
@@ -1,4 +1,7 @@
-﻿using TesteBackendEnContact.Core.Interface.ContactBook;
+﻿using System.Collections.Generic;
+using TesteBackendEnContact.Core.Interface.ContactBook;
+using TesteBackendEnContact.Core.Exceptions;
+using TesteBackendEnContact.Core.Validators;
 
 namespace TesteBackendEnContact.Core.Domain.ContactBook
 {
@@ -6,11 +9,36 @@ namespace TesteBackendEnContact.Core.Domain.ContactBook
     {
         public int Id { get;  set; }
         public string Name { get;  set; }
+        private List<string> _errors;
 
+        protected ContactBook()
+        {
+        }
         public ContactBook(int id, string name)
         {
             Id = id;
             Name = name;
+            _errors = new List<string>();
+        }
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+        public bool Validate()
+        {
+            var validator = new ContactBookValidator();
+            var validation = validator.Validate(this);
+
+            if(!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+                
+                throw new DomainException("Alguns campos estão invalidos, por favor corrija-os!", _errors);
+            }
+
+            return true;
         }
     }
 }
